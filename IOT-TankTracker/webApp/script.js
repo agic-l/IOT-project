23b91af6d481bba852e8c17f627cf8a64607f4e7
agic-l <77613732+agic-l@users.noreply.github.com>
2025-02-22 01:16:21 +0100

    Update script.js

--- IOT-TankTracker/webApp/script.js
+++ IOT-TankTracker/webApp/script.js
@@ -4,13 +4,13 @@ import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/11
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBbM0alNX8Vtl8qo9rA4UlR1VV5mBKkVrE",
-  authDomain: "iot-water-tank-level-project.firebaseapp.com",
-  databaseURL: "https://iot-water-tank-level-project-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "iot-water-tank-level-project",
-  storageBucket: "iot-water-tank-level-project.firebasestorage.app",
-  messagingSenderId: "1073563069923",
-  appId: "1:1073563069923:web:f385fa92333d05303a04d6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
@@ -27,29 +27,6 @@ function updateWaterLevel(level) {
 function updateTankStatus(status) {
   const tankStatusElement = document.getElementById("tankStatus");
   tankStatusElement.textContent = status;
-  //updateDataContainerColor(status); // Update background color based on status
-}
-
-// Function to update .data-container background color based on status
-function updateDataContainerColor(status) {
-  const dataContainer = document.querySelector(".data-container");
-
-  switch (status.toLowerCase()) {
-    case "full":
-      dataContainer.style.backgroundColor = "#99ccff"; // Svijetloplava - označava pun rezervoar
-      break;
-    case "half-full":
-      dataContainer.style.backgroundColor = "#66b3ff"; // Srednje svijetla plava - pola puna
-      break;
-    case "low":
-      dataContainer.style.backgroundColor = "#3399ff"; // Tamnija plava - nizak nivo
-      break;
-    case "empty":
-      dataContainer.style.backgroundColor = "#ffcccc"; // Blago crvenkasta - alarmantno stanje
-      break;
-    default:
-      dataContainer.style.backgroundColor = "#E6F7FF"; // Podrazumijevana boja
-  }
 }
 
 // Listen for changes in the "water_level" data
